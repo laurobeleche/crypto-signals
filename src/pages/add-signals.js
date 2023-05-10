41f44c6b7e1b7de26fa7a0a4ff536e708358c12e
laurobeleche <laurobeleche@gmail.com>
2023-05-10 14:46:59 -0300

    toast e sinais

--- src/pages/add-signals.js
+++ src/pages/add-signals.js
@@ -37,7 +37,7 @@ function AddSignals(props) {
   const addValueToSinais = (data) => {
     // Verifica se dataArray é um array
     let newArray = [...sinais];
-    newArray.unshift(data);
+    newArray.push(data);
 
     // Atualiza o estado com o novo array
     setSinais(newArray);
@@ -58,6 +58,16 @@ function AddSignals(props) {
   // Define a função para esconder o Toast automaticamente
   const hideToast = () => setShowToast(false);
 
+  const isFormValid = () => {
+    return (
+      signalForm.moeda.trim() !== "" &&
+      signalForm.stopLoss.trim() !== "" &&
+      signalForm.entradas.length > 0 &&
+      signalForm.palvos.length > 0
+    );
+  };
+
+
   const [signalForm, setSignalForm] = useState({
     moeda: "",
     precoEntrada: "",
@@ -67,16 +77,9 @@ function AddSignals(props) {
     palvos: [],
   });
   const addEntry = () => {
-    if (signalForm.entradas.length > 0) {
-      const minEntry = Math.min(...signalForm.entradas);
-      if (parseFloat(signalForm.precoEntrada) >= parseFloat(minEntry)) {
-        alert("A nova entrada deve ser menor que as entradas já cadastradas.");
-        return;
-      }
-    }
-
     const newEntries = [...signalForm.entradas, signalForm.precoEntrada];
     setSignalForm({ ...signalForm, entradas: newEntries, precoEntrada: "" });
+    localStorage.setItem("signalForm", JSON.stringify(signalForm));
   };
   const addAlvo = () => {
     if (signalForm.entradas.length > 0) {
@@ -87,26 +90,21 @@ function AddSignals(props) {
       }
     }
 
-    if (signalForm.palvos.length > 0) {
-      const maxAlvo = Math.max(...signalForm.palvos);
-      if (parseFloat(signalForm.alvos) <= parseFloat(maxAlvo)) {
-        alert("O novo alvo deve ser maior que o maior alvo já cadastrado.");
-        return;
-      }
-    }
-
     const newEntries = [...signalForm.palvos, signalForm.alvos];
     setSignalForm({ ...signalForm, palvos: newEntries, alvos: "" });
+    localStorage.setItem("signalForm", JSON.stringify(signalForm));
   };
 
   const removeEntry = (index) => {
     const newEntries = signalForm.entradas.filter((_, i) => i !== index);
     setSignalForm({ ...signalForm, entradas: newEntries });
+    localStorage.setItem("signalForm", JSON.stringify(signalForm));
   };
 
   const removeAlvo = (index) => {
     const newAlvos = signalForm.palvos.filter((_, i) => i !== index);
     setSignalForm({ ...signalForm, palvos: newAlvos });
+    localStorage.setItem("signalForm", JSON.stringify(signalForm));
   };
 
   useEffect(() => {
@@ -118,6 +116,7 @@ function AddSignals(props) {
   const handleInputChange = useCallback((event) => {
     const { name, value } = event.target;
     setSignalForm((prevState) => ({ ...prevState, [name]: value }));
+    localStorage.setItem("signalForm", JSON.stringify(signalForm));
   }, []);
 
   const handleSubmit = async (event) => {
@@ -137,6 +136,7 @@ function AddSignals(props) {
       setToastTitle("Sucesso");
       setToastText(response.data.message);
       setBgClass("bg-success");
+      resetForm(); // Chamar a função resetForm aqui
     } catch (error) {
       setShowToast(true);
       setTimeout(hideToast, 3000); // Fechar o toast após 3 segundos
@@ -145,6 +145,17 @@ function AddSignals(props) {
       setBgClass("bg-danger");
     }
   };
+  const resetForm = () => {
+    setSignalForm({
+      moeda: "",
+      precoEntrada: "",
+      alvos: "",
+      stopLoss: "",
+      entradas: [],
+      palvos: [],
+    });
+  };
+  
 
   return (
     <div style={{ backgroundColor: "#dddddd", paddingBottom: 20 }}>
@@ -160,8 +171,8 @@ function AddSignals(props) {
         <Row className="h-100 text-center" fluid="true">
           {sinais && sinais.length > 0
             ? [...sinais]
-                .reverse()
-                .map((sinal) => <SignalCard key={sinal.id} signal={sinal} />)
+              .reverse()
+              .map((sinal) => <SignalCard key={sinal.id} signal={sinal} />)
             : ""}
         </Row>
         <Row className="h-100">
@@ -176,83 +187,85 @@ function AddSignals(props) {
                     name="moeda"
                     id="moeda"
                     placeholder="Digite a moeda"
+                    value={signalForm.moeda}
                     onChange={handleInputChange}
                   />
                 </FormGroup>
                 <Row fluid="true">
                   <Col xs="6">
-                <FormGroup>
-                  <Label for="precoEntrada">Preço de Entrada</Label>
-                  <Input
-                    type="text"
-                    name="precoEntrada"
-                    id="precoEntrada"
-                    value={signalForm.precoEntrada}
-                    placeholder="Digite o preço de entrada"
-                    onChange={handleInputChange}
-                  />
-                  <Button color="secondary" onClick={addEntry} className="mt-2">
-                    Adicionar Entrada
-                  </Button>
-                  <ListGroup className="mt-2" fluid="true">
-                    {signalForm.entradas.map((entrada, index) => (
-                      <ListGroupItem
-                        key={index}
-                        style={{ maxWidth: 250 }}
-                        className="d-flex justify-content-between"
-                      >
-                        <span>
-                          Entrada {index + 1} - ${entrada}
-                        </span>
-                        <Button
-                          color="danger"
-                          size="sm"
-                          onClick={() => removeEntry(index)}
-                          className="float-right"
-                        >
-                          X
-                        </Button>
-                      </ListGroupItem>
-                    ))}
-                  </ListGroup>
-                </FormGroup>
-                </Col>
-                <Col xs="6">
-                <FormGroup>
-                  <Label for="alvos">Alvos</Label>
-                  <Input
-                    type="text"
-                    name="alvos"
-                    id="alvos"
-                    placeholder="Digite os alvos"
-                    onChange={handleInputChange}
-                  />
-                  <Button color="secondary" onClick={addAlvo} className="mt-2">
-                    Adicionar Alvo
-                  </Button>
-                  <ListGroup className="mt-2" fluid="true">
-                    {signalForm.palvos.map((alvo, index) => (
-                      <ListGroupItem
-                        key={index}
-                        style={{ maxWidth: 250 }}
-                        className="d-flex justify-content-between"
-                      >
-                        <span>
-                          Alvo {index + 1} - ${alvo}
-                        </span>
-                        <Button
-                          color="danger"
-                          size="sm"
-                          onClick={() => removeAlvo(index)}
-                          className="float-right"
-                        >
-                          X
-                        </Button>
-                      </ListGroupItem>
-                    ))}
-                  </ListGroup>
-                </FormGroup>
-                </Col>
+                    <FormGroup>
+                      <Label for="precoEntrada">Preço de Entrada</Label>
+                      <Input
+                        type="text"
+                        name="precoEntrada"
+                        id="precoEntrada"
+                        value={signalForm.precoEntrada}
+                        placeholder="Digite o preço de entrada"
+                        onChange={handleInputChange}
+                      />
+                      <Button color="secondary" onClick={addEntry} className="mt-2">
+                        Adicionar Entrada
+                      </Button>
+                      <ListGroup className="mt-2" fluid="true">
+                        {signalForm.entradas.map((entrada, index) => (
+                          <ListGroupItem
+                            key={index}
+                            style={{ maxWidth: 250 }}
+                            className="d-flex justify-content-between"
+                          >
+                            <span>
+                              Entrada {index + 1} - ${entrada}
+                            </span>
+                            <Button
+                              color="danger"
+                              size="sm"
+                              onClick={() => removeEntry(index)}
+                              className="float-right"
+                            >
+                              X
+                            </Button>
+                          </ListGroupItem>
+                        ))}
+                      </ListGroup>
+                    </FormGroup>
+                  </Col>
+                  <Col xs="6">
+                    <FormGroup>
+                      <Label for="alvos">Alvos</Label>
+                      <Input
+                        type="text"
+                        name="alvos"
+                        id="alvos"
+                        placeholder="Digite os alvos"
+                        value={signalForm.alvos}
+                        onChange={handleInputChange}
+                      />
+                      <Button color="secondary" onClick={addAlvo} className="mt-2">
+                        Adicionar Alvo
+                      </Button>
+                      <ListGroup className="mt-2" fluid="true">
+                        {signalForm.palvos.map((alvo, index) => (
+                          <ListGroupItem
+                            key={index}
+                            style={{ maxWidth: 250 }}
+                            className="d-flex justify-content-between"
+                          >
+                            <span>
+                              Alvo {index + 1} - ${alvo}
+                            </span>
+                            <Button
+                              color="danger"
+                              size="sm"
+                              onClick={() => removeAlvo(index)}
+                              className="float-right"
+                            >
+                              X
+                            </Button>
+                          </ListGroupItem>
+                        ))}
+                      </ListGroup>
+                    </FormGroup>
+                  </Col>
                 </Row>
                 <FormGroup>
                   <Label for="stopLoss">Stop Loss</Label>
@@ -261,12 +274,14 @@ function AddSignals(props) {
                     name="stopLoss"
                     id="stopLoss"
                     placeholder="Digite o valor do Stop Loss"
+                    value={signalForm.stopLoss}
                     onChange={handleInputChange}
                   />
                 </FormGroup>
-                <Button type="submit" color="primary">
+                <Button type="submit" color="primary" disabled={!isFormValid()}>
                   Enviar
                 </Button>
+
               </Form>
             </CardBody>
           </Card>
--- src/pages/components/My-Toast.js
+++ src/pages/components/My-Toast.js
@@ -3,13 +3,16 @@ import { Toast, ToastHeader, ToastBody } from 'reactstrap';
 
 const MyToast = ({ isOpen, title, message, bgClass }) => {
   return (
-    <Toast isOpen={isOpen} className={`${bgClass} text-white position-absolute`} style={{ top: 60, right: 20, zIndex: 1050 }} >
-      <ToastHeader>
-        <strong className="me-auto">{title}</strong>
-      </ToastHeader>
-      <ToastBody>{message}</ToastBody>
-    </Toast>
+    <div style={{ position: 'fixed', top: 60, right: 20, zIndex: 1050 }}>
+      <Toast isOpen={isOpen} className={`${bgClass} text-white`} >
+        <ToastHeader>
+          <strong className="me-auto">{title}</strong>
+        </ToastHeader>
+        <ToastBody>{message}</ToastBody>
+      </Toast>
+    </div>
   );
 };
 
 export default MyToast;
+
