63b2e881d2268dd9c7e1c6ab7882b08413111e6e
Lauro Beleche <laurobeleche@email.com>
2023-05-11 15:39:20 -0300

    cores

--- src/pages/components/Signal-Card.js
+++ src/pages/components/Signal-Card.js
@@ -10,24 +10,27 @@ const SignalCard = ({ signal }) => {
         const date = new Date(dateString);
         return format(date, 'yyyy-MM-dd HH:mm');
       };
+    const colorsCard = ['warning', 'primary', 'primary', 'success', 'danger'];
+    const colorCard = colorsCard[signal.status];
+
   return (
-    <Card className="my-2 p-0 text-center" color="warning" inverse style={{ width: '18rem', margin: 10, maxWidth: 220 }}>
+    <Card className="my-2 p-0 text-center" color={colorCard} inverse style={{ width: '18rem', margin: 10, maxWidth: 220 }}>
         <CardHeader className='p-0'>📌{signal.crypto} / USDT (SPOT)📌</CardHeader>
         <CardBody className='p-0'>
         <ListGroup flush>
-        <ListGroupItem color="warning" className='p-0' >
+        <ListGroupItem color={colorsCard} className='p-0' >
         {formatDate(signal.created_at)}
         </ListGroupItem>
         </ListGroup>
         <CardText></CardText>
         <ListGroup flush>
         <ListGroupItemHeading>
-            <ListGroupItem color="warning" className='p-0' >
+            <ListGroupItem color={colorsCard} className='p-0' >
             ENTRADAS
             </ListGroupItem>
         </ListGroupItemHeading>
         {entradas.map(entrada => (
-            <ListGroupItem color="warning" key={entrada} className='p-0'>
+            <ListGroupItem color={colorsCard} key={entrada} className='p-0'>
             ${entrada}
             </ListGroupItem>
             ))}
@@ -35,18 +38,18 @@ const SignalCard = ({ signal }) => {
         <CardText></CardText>
         <ListGroup flush>
         <ListGroupItemHeading>
-            <ListGroupItem color="warning" className='p-0'>
+            <ListGroupItem color={colorsCard} className='p-0'>
             ALVOS
             </ListGroupItem>
         </ListGroupItemHeading>
         {alvos.map(alvo => (
-            <ListGroupItem color="warning" key={alvo} className='p-0'>
+            <ListGroupItem color={colorsCard} key={alvo} className='p-0'>
             ${alvo}
             </ListGroupItem>
             ))}
         </ListGroup>
         </CardBody>
-        <CardFooter>
+        <CardFooter color={colorsCard}>
         <CardText  className='p-0'>STOP LOSS</CardText>
         <CardText  className='p-0'>😡${signal.stoploss} (-{(((entradas[0] - signal.stoploss)/entradas[0])*100).toFixed(2)}%)</CardText>        
         </CardFooter>
